deb2e44fbc477efcee7628014527e7145da6bd5c
ena so <yso02@uoguelph.ca>
2017-12-17 15:44:08 -0500

    First commit

---
+++ Multipleof3and5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+	int i = 0;
+	int sum = 0;
+	
+	for (i = 1; i < 1000; i++){
+		if (i%3 == 0){
+			sum = sum + i;
+		}
+		else if (i%5 == 0){
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+	return 0;
+}
